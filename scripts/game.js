adf0a974ad2d7fdeb59dfde60340d936917219da
Brian Cristante <none>
2016-12-13 08:28:27 -0800

    Splitting out View and Model

--- scripts/game.js
+++ scripts/game.js
@@ -1,65 +1,96 @@
-var model = function() {
+function Model(width, height) {
+    function flip(tile) {
+        // if the tile is face up
+        //     do nothing
+        // else (tile is face up)
+        //     if this is the first tile
+        //         flip it
+        //         remember what its key is
+        //         move on to the second tile
+        //     else (this is the second tile)
+        //         flip it
+        //         check if its key matches the first's
+        //         if the key matches
+        //             both tiles stay face up
+        //         else
+        //             both tiles are flipped back over
+
+        // TODO
+        tile.revealed = !tile.revealed;
+    }
+
     function Tile(key) {
         this.key = key;
         this.revealed = false;
-    }
-
-    function initializeRow(width) {
-        var row = [];
-        for (let i = 0; i < width; i++) {
-            var key = i;
-            row[i] = new Tile(key);
-        }
-    }
-
-    function initializeBoard(width, height) {
-        var board = [];
-        for (let i = 0; i < height; i++) {
-            board[i] = initializeRow(width);
-        }
-
-        return board;
-    }
-}();
-
-var view = function() {
-    function revealTile() {
-        this.classList.add("face-up");
-        this.onclick = hideTile;
-    }
-
-    //! Invariant: `this` element contains `face-up` in its `classList`.
-    function hideTile() {
-        this.classList.remove("face-up");
-        this.onclick = revealTile;
-    }
-
-    function createTile() {
-        var tile = document.createElement("tile");
-        tile.classList.add("tile");
-        tile.onclick = revealTile;
-
-        return tile;
+        this.notify = () => flip(this);
     }
 
     function createRow(width) {
-        var row = document.createElement("div");
-        row.classList.add("row");
-
-        for (let j = 0; j < width; j++) {
-            row.appendChild(createTile());
+        var row = [];
+        for (let i = 0; i < width; i++) {
+            var key = i; // TODO
+            row[i] = new Tile(key);
         }
 
         return row;
     }
 
-    function createBoard(board, width, height) {
+    function createBoard(width, height) {
+        var board = [];
         for (let i = 0; i < height; i++) {
-            board.appendChild(createRow(width));
+            board[i] = createRow(width);
         }
+
+        return board;
     }
 
-    return {
-        createBoard: createBoard
-    };
-}();
+    this.board = createBoard(width, height);
+    this.width = width;
+    this.height = height;
+};
+
+function createView(model) {
+    function onClickTile(tileView, tileModel) {
+        return () => {
+            tileModel.notify();
+
+            if (tileModel.revealed) {
+                tileView.classList.add("revealed");
+            } else {
+                tileView.classList.remove("revealed");
+            }
+        };
+    }
+
+    function createTile(i, j) {
+        var tile = document.createElement("tile");
+        tile.classList.add("tile");
+        tile.onclick = onClickTile(tile, model.board[i][j]);
+
+        return tile;
+    }
+
+    function createRow(i, width) {
+        var row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < width; j++) {
+            row.appendChild(createTile(i, j));
+        }
+
+        return row;
+    }
+
+    function createBoard(width, height) {
+        var board = document.createElement("div");
+        board.classList.add("board");
+
+        for (let i = 0; i < height; i++) {
+            board.appendChild(createRow(i, width));
+        }
+
+        return board;
+    }
+
+    return createBoard(model.width, model.height);
+};
