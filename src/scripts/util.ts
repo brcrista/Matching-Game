2a85f72c60d78e5becd341fec8d2b8103b0d3dab
Brian Cristante <none>
2017-01-24 09:06:23 -0800

    Compiling TypeScript (with errors). Fixing a bug in the build script where the 'style' directory was not being created. Removing the unused  function in 'util.js'.

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,16 +1,5 @@
 'use strict';
 
-//! Add fields from `parent` to `child` that `child` does not already have.
-function extend(child, parent) {
-    for (let x in parent) {
-        if (x in child) {
-            continue;
-        } else {
-            child[x] = parent[x];
-        }
-    }
-}
-
 //! Produce an array of `n` items from a `generator` function.
 //! The `generator` function will be passed the index of each element being generated.
 function generate(generator, n) {
