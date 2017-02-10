b022c3593e3a0fb31b7780cfcff91429e7da55ee
Brian Cristante <none>
2017-02-09 16:59:10 -0800

    Replacing  functions with built-in array iterator

--- src/scripts/model.ts
+++ src/scripts/model.ts
@@ -1,54 +1,3 @@
-//! Generates keys for tiles on the game board.
-//! The generator should produce each key an even number of times.
-let keyGen = function() {
-    interface GetKey {
-        (heightIndex: number, widthIndex: number): number;
-    }
-
-    interface KeyGenerator {
-        nextKey: GetKey;
-    }
-
-    //! A deterministic generator that assigns each pair of keys to adjacent tiles.
-    class SideBySideGenerator implements KeyGenerator {
-        nextKey: GetKey;
-
-        constructor(height: number, width: number) {
-            this.nextKey = function(heightIndex, widthIndex) {
-                const tileNumber = heightIndex * height + widthIndex;
-                return Math.floor(tileNumber / 2);
-            }
-        };
-    }
-
-    //! A generator that assigns keys by choosing them with a uniform distribution.
-    class UniformRandomGenerator implements KeyGenerator {
-        nextKey: GetKey;
-        private remainingKeys: number[];
-
-        constructor(height: number, width: number) {
-            const numberOfTiles = height * width;
-            if (numberOfTiles % 2 !== 0) throw new RangeError(`${height} * ${width} is not divisible by 2`);
-
-            const keyRange = range(0, numberOfTiles / 2 - 1);
-            this.remainingKeys = keyRange.concat(keyRange);
-
-            this.nextKey = function(heightIndex, widthIndex): number {
-                //! Randomly choose an integer in the range [`0`, `size`) with a uniform distribution.
-                function randomIndex(size) {
-                    return Math.floor(Math.random() * size);
-                }
-
-                return remove<number>(this.remainingKeys, randomIndex(this.remainingKeys.length));
-            };
-        }
-    }
-
-    return {
-        UniformRandomGenerator: UniformRandomGenerator
-    };
-}();
-
 let model = function() {
     class GameState {
         firstTry: boolean;
@@ -141,10 +90,11 @@ let model = function() {
             function createBoard(width, height, gameState) {
                 let board = generate<Tile[]>(createRow.bind(null, width, gameState), height);
 
-                let keys = new keyGen.UniformRandomGenerator(height, width);
+                let keys = range(0, width * height - 1).map((n) => Math.floor(n / 2));
+                let keyIterator = randomize(keys)[Symbol.iterator]();
                 for (let i = 0; i < height; i++) {
                     for (let j = 0; j < width; j++) {
-                        board[i][j].key = keys.nextKey(i, j);
+                        board[i][j].key = keyIterator.next().value;
                     }
                 }
 
--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,6 +1,6 @@
 //! Produce an array of `n` items from a `generator` function.
 //! The `generator` function will be passed the index of each element being generated.
-function generate<T>(generator: (i?: number) => T, n: number): Array<T> {
+function generate<T>(generator: (i?: number) => T, n: number): T[] {
     let result: T[] = [];
     for (let i = 0; i < n; i++) {
         result.push(generator(i));
@@ -13,7 +13,7 @@ function generate<T>(generator: (i?: number) => T, n: number): Array<T> {
 //! If `min` is not an integer it is rounded up.
 //! If `max` is not an integer it is rounded down.
 //! If `min` > `max` (after rounding if `min` or `max` is not an integer), an empty array is returned.
-function range(min: number, max: number): Array<number> {
+function range(min: number, max: number): number[] {
     min = Math.ceil(min);
     max = Math.floor(max);
 
@@ -26,4 +26,9 @@ function remove<T>(arr: Array<T>, i: number): T {
     if (!(i in range(0, arr.length - 1))) throw new RangeError(`${i} is not in the range [0, ${arr.length})`);
 
     return arr.splice(i, 1)[0];
+}
+
+//! Create a random permutation of array `arr`.
+function randomize<T>(arr: T[]): T[] {
+    return arr; // TODO
 }
